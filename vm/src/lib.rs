e49dd44afcd014b86935aa397168034d455b9295
Obito-git <antonmyroshnychenko@gmail.com>
2025-08-18 22:51:25 +0200

    making a lot of stubs for print hello world, and sketching the runtime

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -5,4 +5,24 @@ mod native_registry;
 pub mod stack;
 mod string_pool;
 
+type HeapAddr = usize;
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    Null,
+    Ref(HeapAddr),
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct ClassId(pub usize);
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct MethodId(pub usize);
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ClassState {
+    Loaded,
+    Initialized,
+}
+
 pub fn run(_main: Vec<u8>) {}
--- vm/src/string_pool.rs
+++ vm/src/string_pool.rs
@@ -1,7 +1,27 @@
-pub struct StringPool;
+use crate::heap::Heap;
+use crate::HeapAddr;
+use std::collections::HashMap;
+
+pub struct StringPool {
+    pool: HashMap<String, HeapAddr>,
+}
 
 impl StringPool {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            pool: HashMap::new(),
+        }
+    }
+    pub fn get_or_new(&mut self, heap: &mut Heap, text: &str) -> HeapAddr {
+        if let Some(&h) = self.pool.get(text) {
+            return h;
+        }
+        let h = heap.alloc_string(text);
+        self.pool.insert(text.to_string(), h);
+        h
+    }
+
+    pub fn contains(&self, s: &str) -> bool {
+        self.pool.contains_key(s)
     }
 }
