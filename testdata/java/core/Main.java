72e8799881be2faab20626f04f45ba98e3cf782c
Obito-git <antonmyroshnychenko@gmail.com>
2025-08-26 20:32:33 +0200

    add basic pipeline, including an infrastructure for tests

---
+++ testdata/java/core/Main.java
@@ -0,0 +1,9 @@
+package fixtures.core;
+
+public class Main extends Object{
+    public static void main(String[] args) {
+        var a = "Hello, World!";
+        var b = "Hello, World!";
+        System.out.println("Hello, World!");
+    }
+}
