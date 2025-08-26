6f0912ee92b114ca01d634e210984a85f7fa1602
Obito-git <antonmyroshnychenko@gmail.com>
2025-08-26 20:39:22 +0200

    disable temporary the format checking and fix small problems with the workflow

--- testdata/java/core/Main.java
+++ testdata/java/core/Main.java
@@ -1,9 +1,7 @@
 package fixtures.core;
 
-public class Main extends Object{
+public class Main {
     public static void main(String[] args) {
-        var a = "Hello, World!";
-        var b = "Hello, World!";
         System.out.println("Hello, World!");
     }
 }
