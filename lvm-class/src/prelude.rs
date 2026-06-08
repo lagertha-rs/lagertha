b2912a5f692ef1de8166f01dd5a78dd42910c44b
Obito-git <antonmyroshnychenko@gmail.com>
2026-06-08 19:27:48 +0200

    rename rns_assemble feature to write. in lvm-class we shouldn't have any rns domain naming

--- lvm-class/src/prelude.rs
+++ lvm-class/src/prelude.rs
@@ -15,10 +15,10 @@ pub use crate::ClassFile;
 // Constant pool
 pub use crate::constant_pool::{ConstantEntry, ConstantKind, ConstantPool};
 pub use crate::constant_pool::{Dynamic, MethodHandle, MethodHandleKind, NameAndType, Reference};
-#[cfg(feature = "rns_assemble")]
-pub use crate::rns_asm::builder::ConstantPoolBuilder;
-#[cfg(feature = "rns_assemble")]
-pub use crate::rns_asm::class_file::ClassFileBuilder;
+#[cfg(feature = "write")]
+pub use crate::write::builder::ConstantPoolBuilder;
+#[cfg(feature = "write")]
+pub use crate::write::class_file::ClassFileBuilder;
 
 // Members
 pub use crate::member::{FieldInfo, MethodInfo};
@@ -32,8 +32,8 @@ pub use crate::attribute::{
     ElementValuePair, FieldAttribute, LocalVarEntry, MethodAttribute, SharedAttribute, TargetInfo,
     TypeAnnotation, TypePath, TypePathEntry,
 };
-#[cfg(feature = "rns_assemble")]
-pub use crate::rns_asm::AttributeNameMap;
+#[cfg(feature = "write")]
+pub use crate::write::AttributeNameMap;
 
 // Flags
 pub use crate::flags::{ClassFlags, FieldFlags, InnerClassFlags, MethodFlags, MethodParamFlags};
