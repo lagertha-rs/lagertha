62c8a86774046c0e1f70551a270de4fcb1997c73
Obito-git <antonmyroshnychenko@gmail.com>
2026-07-09 20:39:08 +0200

    Revert "share attr str constants"
    This reverts commit 11c3d48914c9e2b76938911de150da06a7e60f5b.

--- lvm-class/src/attribute/mod.rs
+++ lvm-class/src/attribute/mod.rs
@@ -22,39 +22,6 @@ pub use method::{
 pub use shared::SharedAttribute;
 pub use type_annotation::{LocalVarEntry, TargetInfo, TypeAnnotation, TypePath, TypePathEntry};
 
-pub const ATTR_CONSTANT_VALUE: &'static str = "ConstantValue";
-pub const ATTR_CODE: &'static str = "Code";
-pub const ATTR_EXCEPTIONS: &'static str = "Exceptions";
-pub const ATTR_SOURCE_FILE: &'static str = "SourceFile";
-pub const ATTR_LINE_NUMBER_TABLE: &'static str = "LineNumberTable";
-pub const ATTR_LOCAL_VARIABLE_TABLE: &'static str = "LocalVariableTable";
-pub const ATTR_INNER_CLASSES: &'static str = "InnerClasses";
-pub const ATTR_SYNTHETIC: &'static str = "Synthetic";
-pub const ATTR_DEPRECATED: &'static str = "Deprecated";
-pub const ATTR_ENCLOSING_METHOD: &'static str = "EnclosingMethod";
-pub const ATTR_SIGNATURE: &'static str = "Signature";
-pub const ATTR_SOURCE_DEBUG_EXTENSION: &'static str = "SourceDebugExtension";
-pub const ATTR_LOCAL_VARIABLE_TYPE_TABLE: &'static str = "LocalVariableTypeTable";
-pub const ATTR_RUNTIME_VISIBLE_ANNOTATIONS: &'static str = "RuntimeVisibleAnnotations";
-pub const ATTR_RUNTIME_INVISIBLE_ANNOTATIONS: &'static str = "RuntimeInvisibleAnnotations";
-pub const ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS: &'static str =
-    "RuntimeVisibleParameterAnnotations";
-pub const ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS: &'static str =
-    "RuntimeInvisibleParameterAnnotations";
-pub const ATTR_ANNOTATION_DEFAULT: &'static str = "AnnotationDefault";
-pub const ATTR_STACK_MAP_TABLE: &'static str = "StackMapTable";
-pub const ATTR_BOOTSTRAP_METHODS: &'static str = "BootstrapMethods";
-pub const ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS: &'static str = "RuntimeVisibleTypeAnnotations";
-pub const ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS: &'static str = "RuntimeInvisibleTypeAnnotations";
-pub const ATTR_METHOD_PARAMETERS: &'static str = "MethodParameters";
-pub const ATTR_MODULE: &'static str = "Module";
-pub const ATTR_MODULE_PACKAGES: &'static str = "ModulePackages";
-pub const ATTR_MODULE_MAIN_CLASS: &'static str = "ModuleMainClass";
-pub const ATTR_NEST_HOST: &'static str = "NestHost";
-pub const ATTR_NEST_MEMBERS: &'static str = "NestMembers";
-pub const ATTR_RECORD: &'static str = "Record";
-pub const ATTR_PERMITTED_SUBCLASSES: &'static str = "PermittedSubclasses";
-
 /// Discriminant for attribute types defined in the JVM specification.
 ///
 /// https://docs.oracle.com/javase/specs/jvms/se25/html/jvms-4.html#jvms-4.7
@@ -93,41 +60,76 @@ pub enum AttributeKind {
 }
 
 impl AttributeKind {
+    const ATTR_CONSTANT_VALUE: &'static str = "ConstantValue";
+    const ATTR_CODE: &'static str = "Code";
+    const ATTR_EXCEPTIONS: &'static str = "Exceptions";
+    const ATTR_SOURCE_FILE: &'static str = "SourceFile";
+    const ATTR_LINE_NUMBER_TABLE: &'static str = "LineNumberTable";
+    const ATTR_LOCAL_VARIABLE_TABLE: &'static str = "LocalVariableTable";
+    const ATTR_INNER_CLASSES: &'static str = "InnerClasses";
+    const ATTR_SYNTHETIC: &'static str = "Synthetic";
+    const ATTR_DEPRECATED: &'static str = "Deprecated";
+    const ATTR_ENCLOSING_METHOD: &'static str = "EnclosingMethod";
+    const ATTR_SIGNATURE: &'static str = "Signature";
+    const ATTR_SOURCE_DEBUG_EXTENSION: &'static str = "SourceDebugExtension";
+    const ATTR_LOCAL_VARIABLE_TYPE_TABLE: &'static str = "LocalVariableTypeTable";
+    const ATTR_RUNTIME_VISIBLE_ANNOTATIONS: &'static str = "RuntimeVisibleAnnotations";
+    const ATTR_RUNTIME_INVISIBLE_ANNOTATIONS: &'static str = "RuntimeInvisibleAnnotations";
+    const ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS: &'static str =
+        "RuntimeVisibleParameterAnnotations";
+    const ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS: &'static str =
+        "RuntimeInvisibleParameterAnnotations";
+    const ATTR_ANNOTATION_DEFAULT: &'static str = "AnnotationDefault";
+    const ATTR_STACK_MAP_TABLE: &'static str = "StackMapTable";
+    const ATTR_BOOTSTRAP_METHODS: &'static str = "BootstrapMethods";
+    const ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS: &'static str = "RuntimeVisibleTypeAnnotations";
+    const ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS: &'static str = "RuntimeInvisibleTypeAnnotations";
+    const ATTR_METHOD_PARAMETERS: &'static str = "MethodParameters";
+    const ATTR_MODULE: &'static str = "Module";
+    const ATTR_MODULE_PACKAGES: &'static str = "ModulePackages";
+    const ATTR_MODULE_MAIN_CLASS: &'static str = "ModuleMainClass";
+    const ATTR_NEST_HOST: &'static str = "NestHost";
+    const ATTR_NEST_MEMBERS: &'static str = "NestMembers";
+    const ATTR_RECORD: &'static str = "Record";
+    const ATTR_PERMITTED_SUBCLASSES: &'static str = "PermittedSubclasses";
+
     #[inline]
     pub const fn as_str(&self) -> &'static str {
         match self {
-            Self::ConstantValue => ATTR_CONSTANT_VALUE,
-            Self::Code => ATTR_CODE,
-            Self::Exceptions => ATTR_EXCEPTIONS,
-            Self::SourceFile => ATTR_SOURCE_FILE,
-            Self::LineNumberTable => ATTR_LINE_NUMBER_TABLE,
-            Self::LocalVariableTable => ATTR_LOCAL_VARIABLE_TABLE,
-            Self::InnerClasses => ATTR_INNER_CLASSES,
-            Self::Synthetic => ATTR_SYNTHETIC,
-            Self::Deprecated => ATTR_DEPRECATED,
-            Self::EnclosingMethod => ATTR_ENCLOSING_METHOD,
-            Self::Signature => ATTR_SIGNATURE,
-            Self::SourceDebugExtension => ATTR_SOURCE_DEBUG_EXTENSION,
-            Self::LocalVariableTypeTable => ATTR_LOCAL_VARIABLE_TYPE_TABLE,
-            Self::RuntimeVisibleAnnotations => ATTR_RUNTIME_VISIBLE_ANNOTATIONS,
-            Self::RuntimeInvisibleAnnotations => ATTR_RUNTIME_INVISIBLE_ANNOTATIONS,
-            Self::RuntimeVisibleParameterAnnotations => ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS,
-            Self::RuntimeInvisibleParameterAnnotations => {
-                ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS
+            Self::ConstantValue => Self::ATTR_CONSTANT_VALUE,
+            Self::Code => Self::ATTR_CODE,
+            Self::Exceptions => Self::ATTR_EXCEPTIONS,
+            Self::SourceFile => Self::ATTR_SOURCE_FILE,
+            Self::LineNumberTable => Self::ATTR_LINE_NUMBER_TABLE,
+            Self::LocalVariableTable => Self::ATTR_LOCAL_VARIABLE_TABLE,
+            Self::InnerClasses => Self::ATTR_INNER_CLASSES,
+            Self::Synthetic => Self::ATTR_SYNTHETIC,
+            Self::Deprecated => Self::ATTR_DEPRECATED,
+            Self::EnclosingMethod => Self::ATTR_ENCLOSING_METHOD,
+            Self::Signature => Self::ATTR_SIGNATURE,
+            Self::SourceDebugExtension => Self::ATTR_SOURCE_DEBUG_EXTENSION,
+            Self::LocalVariableTypeTable => Self::ATTR_LOCAL_VARIABLE_TYPE_TABLE,
+            Self::RuntimeVisibleAnnotations => Self::ATTR_RUNTIME_VISIBLE_ANNOTATIONS,
+            Self::RuntimeInvisibleAnnotations => Self::ATTR_RUNTIME_INVISIBLE_ANNOTATIONS,
+            Self::RuntimeVisibleParameterAnnotations => {
+                Self::ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS
             }
-            Self::AnnotationDefault => ATTR_ANNOTATION_DEFAULT,
-            Self::StackMapTable => ATTR_STACK_MAP_TABLE,
-            Self::BootstrapMethods => ATTR_BOOTSTRAP_METHODS,
-            Self::RuntimeVisibleTypeAnnotations => ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS,
-            Self::RuntimeInvisibleTypeAnnotations => ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS,
-            Self::MethodParameters => ATTR_METHOD_PARAMETERS,
-            Self::Module => ATTR_MODULE,
-            Self::ModulePackages => ATTR_MODULE_PACKAGES,
-            Self::ModuleMainClass => ATTR_MODULE_MAIN_CLASS,
-            Self::NestHost => ATTR_NEST_HOST,
-            Self::NestMembers => ATTR_NEST_MEMBERS,
-            Self::Record => ATTR_RECORD,
-            Self::PermittedSubclasses => ATTR_PERMITTED_SUBCLASSES,
+            Self::RuntimeInvisibleParameterAnnotations => {
+                Self::ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS
+            }
+            Self::AnnotationDefault => Self::ATTR_ANNOTATION_DEFAULT,
+            Self::StackMapTable => Self::ATTR_STACK_MAP_TABLE,
+            Self::BootstrapMethods => Self::ATTR_BOOTSTRAP_METHODS,
+            Self::RuntimeVisibleTypeAnnotations => Self::ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS,
+            Self::RuntimeInvisibleTypeAnnotations => Self::ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS,
+            Self::MethodParameters => Self::ATTR_METHOD_PARAMETERS,
+            Self::Module => Self::ATTR_MODULE,
+            Self::ModulePackages => Self::ATTR_MODULE_PACKAGES,
+            Self::ModuleMainClass => Self::ATTR_MODULE_MAIN_CLASS,
+            Self::NestHost => Self::ATTR_NEST_HOST,
+            Self::NestMembers => Self::ATTR_NEST_MEMBERS,
+            Self::Record => Self::ATTR_RECORD,
+            Self::PermittedSubclasses => Self::ATTR_PERMITTED_SUBCLASSES,
         }
     }
 }
@@ -137,38 +139,40 @@ impl TryFrom<&str> for AttributeKind {
 
     fn try_from(s: &str) -> Result<Self, Self::Error> {
         Ok(match s {
-            ATTR_CONSTANT_VALUE => Self::ConstantValue,
-            ATTR_CODE => Self::Code,
-            ATTR_EXCEPTIONS => Self::Exceptions,
-            ATTR_SOURCE_FILE => Self::SourceFile,
-            ATTR_LINE_NUMBER_TABLE => Self::LineNumberTable,
-            ATTR_LOCAL_VARIABLE_TABLE => Self::LocalVariableTable,
-            ATTR_INNER_CLASSES => Self::InnerClasses,
-            ATTR_SYNTHETIC => Self::Synthetic,
-            ATTR_DEPRECATED => Self::Deprecated,
-            ATTR_ENCLOSING_METHOD => Self::EnclosingMethod,
-            ATTR_SIGNATURE => Self::Signature,
-            ATTR_SOURCE_DEBUG_EXTENSION => Self::SourceDebugExtension,
-            ATTR_LOCAL_VARIABLE_TYPE_TABLE => Self::LocalVariableTypeTable,
-            ATTR_RUNTIME_VISIBLE_ANNOTATIONS => Self::RuntimeVisibleAnnotations,
-            ATTR_RUNTIME_INVISIBLE_ANNOTATIONS => Self::RuntimeInvisibleAnnotations,
-            ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS => Self::RuntimeVisibleParameterAnnotations,
-            ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS => {
+            Self::ATTR_CONSTANT_VALUE => Self::ConstantValue,
+            Self::ATTR_CODE => Self::Code,
+            Self::ATTR_EXCEPTIONS => Self::Exceptions,
+            Self::ATTR_SOURCE_FILE => Self::SourceFile,
+            Self::ATTR_LINE_NUMBER_TABLE => Self::LineNumberTable,
+            Self::ATTR_LOCAL_VARIABLE_TABLE => Self::LocalVariableTable,
+            Self::ATTR_INNER_CLASSES => Self::InnerClasses,
+            Self::ATTR_SYNTHETIC => Self::Synthetic,
+            Self::ATTR_DEPRECATED => Self::Deprecated,
+            Self::ATTR_ENCLOSING_METHOD => Self::EnclosingMethod,
+            Self::ATTR_SIGNATURE => Self::Signature,
+            Self::ATTR_SOURCE_DEBUG_EXTENSION => Self::SourceDebugExtension,
+            Self::ATTR_LOCAL_VARIABLE_TYPE_TABLE => Self::LocalVariableTypeTable,
+            Self::ATTR_RUNTIME_VISIBLE_ANNOTATIONS => Self::RuntimeVisibleAnnotations,
+            Self::ATTR_RUNTIME_INVISIBLE_ANNOTATIONS => Self::RuntimeInvisibleAnnotations,
+            Self::ATTR_RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS => {
+                Self::RuntimeVisibleParameterAnnotations
+            }
+            Self::ATTR_RUNTIME_INVISIBLE_PARAMETER_ANNOTATIONS => {
                 Self::RuntimeInvisibleParameterAnnotations
             }
-            ATTR_ANNOTATION_DEFAULT => Self::AnnotationDefault,
-            ATTR_STACK_MAP_TABLE => Self::StackMapTable,
-            ATTR_BOOTSTRAP_METHODS => Self::BootstrapMethods,
-            ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS => Self::RuntimeVisibleTypeAnnotations,
-            ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS => Self::RuntimeInvisibleTypeAnnotations,
-            ATTR_METHOD_PARAMETERS => Self::MethodParameters,
-            ATTR_MODULE => Self::Module,
-            ATTR_MODULE_PACKAGES => Self::ModulePackages,
-            ATTR_MODULE_MAIN_CLASS => Self::ModuleMainClass,
-            ATTR_NEST_HOST => Self::NestHost,
-            ATTR_NEST_MEMBERS => Self::NestMembers,
-            ATTR_RECORD => Self::Record,
-            ATTR_PERMITTED_SUBCLASSES => Self::PermittedSubclasses,
+            Self::ATTR_ANNOTATION_DEFAULT => Self::AnnotationDefault,
+            Self::ATTR_STACK_MAP_TABLE => Self::StackMapTable,
+            Self::ATTR_BOOTSTRAP_METHODS => Self::BootstrapMethods,
+            Self::ATTR_RUNTIME_VISIBLE_TYPE_ANNOTATIONS => Self::RuntimeVisibleTypeAnnotations,
+            Self::ATTR_RUNTIME_INVISIBLE_TYPE_ANNOTATIONS => Self::RuntimeInvisibleTypeAnnotations,
+            Self::ATTR_METHOD_PARAMETERS => Self::MethodParameters,
+            Self::ATTR_MODULE => Self::Module,
+            Self::ATTR_MODULE_PACKAGES => Self::ModulePackages,
+            Self::ATTR_MODULE_MAIN_CLASS => Self::ModuleMainClass,
+            Self::ATTR_NEST_HOST => Self::NestHost,
+            Self::ATTR_NEST_MEMBERS => Self::NestMembers,
+            Self::ATTR_RECORD => Self::Record,
+            Self::ATTR_PERMITTED_SUBCLASSES => Self::PermittedSubclasses,
             _ => return Err(ClassFormatErr::UnknownAttribute(s.to_string())),
         })
     }
